ccd930a1bcad8c915bcef10f949e02e9edfa3352
Husen Mansurov <lirvess@gmail.com>
2021-01-23 19:48:29 +0900

    2 commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -16,7 +16,10 @@ public class Main {
         UserDaoJDBCImpl ud = new UserDaoJDBCImpl();
         User user = new User();
         ud.createUsersTable();
-        ud.saveUser("Ivan", "Popov", (byte) 21);
+        ud.saveUser("Ivan", "Popov", (byte) 27);
+        ud.saveUser("Niko", "Kozlov", (byte) 21);
+        ud.saveUser("Alex", "Lonov", (byte) 35);
+        ud.saveUser("Mike", "Stasov", (byte) 75);
         System.out.println(ud.getAllUsers());
         ud.cleanUsersTable();
         ud.dropUsersTable();
